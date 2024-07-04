0052a7ae66343bb02a5e6faab86721dafe6b7252
XelXen <78258352+XelXen@users.noreply.github.com>
2024-07-04 14:04:09 +0530

    Basic Help message and posting implementation with a few backend improvements

--- database.py
+++ database.py
@@ -2,51 +2,85 @@
 
 import pickle
 import typing
-import varfile
+import config
 import hashlib
 import random
 
 
 # Define Database Type
 
-class Database(typing.TypedDict):
-    banned_users: typing.Set[int]
-    msg_list: typing.List[int]
+
+class DatabaseType(typing.TypedDict):
+    like_ratio: typing.Dict[int, int]
     user_timings: typing.Dict[int, int]
-    images: typing.Dict[int, str]
+    autodelete: typing.Set[int]
 
 
 # Database Core Functions
 
-def save(db: Database, name: str = varfile.DATABASE_FILE) -> None:
+
+def save(db: DatabaseType, name: str = config.DATABASE_FILE) -> None:
     with open(file=name, mode="wb") as f:
         pickle.dump(obj=db, file=f)
 
 
-def load(name: str = varfile.DATABASE_FILE) -> Database:
+def load(name: str = config.DATABASE_FILE) -> DatabaseType:
     try:
         with open(file=name, mode="rb") as f:
-            return pickle.load(file=f)
-        
+            db: DatabaseType = pickle.load(file=f)
+            return db
+
     except FileNotFoundError:
-        db: Database = {
-            "banned_users": set(),
-            "msg_list": [],
-            "user_timings": {},
-            "images": {}
+        db: DatabaseType = {
+            "like_ratio": dict(),
+            "user_timings": dict(),
+            "autodelete": set(),
         }
 
         save(db=db)
-        
+
         return db
+
+
+# Define Database class
+
+
+class DatabaseSession:
+    def __init__(self) -> None:
+        self.db = load()
+
+    def __getitem__(self, key) -> typing.Any:
+        return self.db[key] if key in self.db else None
+
+    def __setitem__(self, key, value) -> None:
+        self.db[key] = value
+        save(db=self.db)
+
+    def __delitem__(self, key) -> None:
+        del self.db[key]
+        save(db=self.db)
+
+    def __contains__(self, key) -> bool:
+        return key in self.db
+
+    def __iter__(self) -> typing.Iterator:
+        return iter(self.db)
+
+    def reload(self) -> None:
+        self.db = load()
 
 
 # Decorator Functions
 
+
 def sync(func: typing.Callable) -> typing.Callable:
     def wrapper(*args, **kwargs) -> None:
-        db: Database = load()
-        result = func(db=db, *args, **kwargs) if "db" in func.__code__.co_varnames and "db" not in kwargs else func(*args, **kwargs)
+        db: DatabaseType = load()
+        result = (
+            func(db=db, *args, **kwargs)
+            if "db" in func.__code__.co_varnames and "db" not in kwargs
+            else func(*args, **kwargs)
+        )
         save(db=result)
 
     return wrapper
@@ -54,8 +88,12 @@ def sync(func: typing.Callable) -> typing.Callable:
 
 def syncn(func: typing.Callable) -> typing.Callable:
     def wrapper(*args, **kwargs) -> typing.Any:
-        db: Database = load()
-        result = func(db=db, *args, **kwargs) if "db" in func.__code__.co_varnames and "db" not in kwargs else func(*args, **kwargs)
+        db: DatabaseType = load()
+        result = (
+            func(db=db, *args, **kwargs)
+            if "db" in func.__code__.co_varnames and "db" not in kwargs
+            else func(*args, **kwargs)
+        )
         return result
 
     return wrapper
@@ -63,11 +101,16 @@ def syncn(func: typing.Callable) -> typing.Callable:
 
 # Sugarcoated Functions
 
+
 def hash_user(user_id: int) -> str:
     seed = random.randint(a=0, b=100000)
-    return hashlib.md5(string=str(user_id+seed).encode()).hexdigest() + str(seed).zfill(6)
+    return hashlib.md5(string=str(user_id + seed).encode()).hexdigest() + str(
+        seed
+    ).zfill(6)
 
 
 def verify_user(user_id: int, user_hash: str) -> bool:
     seed = int(user_hash[-6:])
-    return user_hash == hashlib.md5(string=str(user_id+seed).encode()).hexdigest() + str(seed).zfill(6)
+    return user_hash == hashlib.md5(
+        string=str(user_id + seed).encode()
+    ).hexdigest() + str(seed).zfill(6)
--- main.py
+++ main.py
@@ -1,12 +1,193 @@
 # Import Core Libraries
 import pyrogram
 import time
-import varfile
+import config
 import database
+import os
+
+from pyrogram import filters
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    CallbackQuery,
+    Message,
+)
+from pyrogram.enums.parse_mode import ParseMode
 
 
-# Initialize Pyrogram Client
-app = pyrogram.Client(name=varfile.NAME, api_id=varfile.API_ID, api_hash=varfile.API_HASH, bot_token=varfile.BOT_TOKEN)
+# Initialize Pyrogram Client and Database
+
+app = pyrogram.Client(
+    name=config.NAME,
+    api_id=config.API_ID,
+    api_hash=config.API_HASH,
+    bot_token=config.BOT_TOKEN,
+)
+
+db = database.DatabaseSession()
 
 
-# Define Core Functions
+# Define Callback Functions
+
+
+@app.on_message(filters=filters.command(commands=["start"]))
+async def start(_: pyrogram.Client, message: Message) -> None:
+    if len(message.command) == 1:
+        await message.reply_text(
+            text=(
+                "Hello there! I am TG-Chan Posting Bot. I can help you post anonymous messages to TG-Chan.\n\n"
+                "Command List:\n"
+                "/start - Introduction & command list\n"
+                "/post - Post an anonymous message (use as a reply to the message)\n"
+                "/delete - Delete a message\n"
+                "/hash - Get your unique hash id (SECRET - DO NOT SHARE!)\n"
+                "/privacy - Get Privacy Policy of the bot"
+            ),
+            parse_mode=ParseMode.DISABLED,
+        )
+    else:
+        key = message.command[1]
+
+        file_path = f"media/{key[3:]}.{key[:3]}"
+        if not os.path.exists(path=file_path):
+            await message.reply_text(
+                text=("Invalid media key! Please try again with a valid media key.")
+            )
+
+            return
+
+        if key.startswith("img"):
+            await message.reply_photo(
+                photo=file_path,
+                caption="Here is the media you requested.",
+            )
+
+        elif key.startswith("mp4"):
+            await message.reply_video(
+                video=file_path,
+                caption="Here is the media you requested.",
+            )
+
+
+@app.on_message(filters=filters.command(commands=["post"]) & filters.reply)
+async def post(client: pyrogram.Client, message: Message) -> None:
+    db.reload()
+
+    if message.from_user.id in db["user_timings"]:
+        if (
+            time.time() - db["user_timings"][message.from_user.id]
+            < config.POST_INTERVAL
+        ):
+            await message.reply_text(
+                text=(
+                    "You are posting too fast! Please wait for a while before posting again."
+                )
+            )
+
+            return
+
+    # TODO : Clear the existing autodelete queue if it exceeds the limit
+
+    message = message.reply_to_message
+
+    if message.photo:
+        if message.photo.file_size > config.MAX_IMAGE_SIZE:
+            await message.reply_text(
+                text=(
+                    "The image size is too large! Please try again with a smaller/compressed image or add a link to the image instead."
+                )
+            )
+
+            return
+
+        await message.download(file_name=f"media/{message.photo.file_id}.jpg")
+        command = f"https://t.me/{config.BOT_USERNAME}?start=img{message.photo.file_id}"
+        caption = message.caption if message.caption else ""
+
+        msg = await client.send_message(
+            chat_id=config.POST_ID,
+            text=caption
+            + f"\n\n[Click here to view the photo]({command})"
+            + f"\n\nHash: {database.hash_user(user_id=message.from_user.id)}",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text="👍",
+                            callback_data="like",
+                        ),
+                        InlineKeyboardButton(
+                            text="👎",
+                            callback_data="dislike",
+                        ),
+                    ],
+                ],
+            ),
+        )
+
+    elif message.video:
+        if message.video.file_size > config.MAX_VIDEO_SIZE:
+            await message.reply_text(
+                text=(
+                    "The video size is too large! Please try again with a smaller/compressed video or add a link to the video instead."
+                )
+            )
+
+            return
+
+        await message.download(file_name=f"media/{message.video.file_id}.mp4")
+        command = f"https://t.me/{config.BOT_USERNAME}?start=mp4{message.video.file_id}"
+        caption = message.caption if message.caption else ""
+
+        msg = await client.send_message(
+            chat_id=config.POST_ID,
+            text=caption
+            + f"\n\n[Click here to view the video]({command})"
+            + f"\n\nHash: {database.hash_user(user_id=message.from_user.id)}",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text="👍",
+                            callback_data="like",
+                        ),
+                        InlineKeyboardButton(
+                            text="👎",
+                            callback_data="dislike",
+                        ),
+                    ],
+                ],
+            ),
+        )
+
+    elif message.text:
+        await client.send_message(
+            chat_id=config.POST_ID,
+            text=message.text
+            + f"\n\nHash: {database.hash_user(user_id=message.from_user.id)}",
+            reply_markup=InlineKeyboardMarkup(
+                inline_keyboard=[
+                    [
+                        InlineKeyboardButton(
+                            text="👍",
+                            callback_data="like",
+                        ),
+                        InlineKeyboardButton(
+                            text="👎",
+                            callback_data="dislike",
+                        ),
+                    ],
+                ],
+            ),
+        )
+
+    else:
+        await message.reply_text(
+            text=("Invalid message type! Please try again with a valid message type.")
+        )
+
+        return
+
+    db["like_ratio"][msg.id] = 0
+    db["user_timings"][message.from_user.id] = time.time()
+    db["autodelete"].add(msg.id)
